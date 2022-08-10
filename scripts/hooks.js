5cb041cac1ce2582440027afafab54480e303947
Mangó <mango.shadowplay@gmail.com>
2022-08-10 22:18:41 +0200

    Add initialising log

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -1,4 +1,4 @@
-import { MODULE_NAME } from "./utils/const.js";
+import { MODULE_TITLE } from "./utils/const.js";
 import { LogUtility } from "./utils/log.js";
 import { SettingsUtility } from "./settings.js";
 
@@ -6,6 +6,7 @@ export class HooksUtility {
     static registerHooks() {
         // Registers all module settings
         Hooks.once("init", () => {
+            LogUtility.log(`Initialising ${MODULE_TITLE}`)
             SettingsUtility.registerSettings();
         });
 
