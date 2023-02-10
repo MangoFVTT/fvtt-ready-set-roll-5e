9825a776a4c2ba45ab206cbfe1a26092c0c50d35
Mangó <mango.shadowplay@gmail.com>
2023-02-10 17:23:30 +0100

    Add template to template list

--- src/module/templates.js
+++ src/module/templates.js
@@ -15,5 +15,6 @@ export const TEMPLATE = {
     OPTIONS: "rsr-item-options.html",
     BLANK: "rsr-blank.html",
     OVERLAY_DAMAGE: "rsr-overlay-damage.html",
-    OVERLAY_MULTIROLL: "rsr-overlay-multiroll.html"
+    OVERLAY_MULTIROLL: "rsr-overlay-multiroll.html",
+    QUERY_DIALOG: "rsr-query-dialog.html"
 }
