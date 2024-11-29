84b03ec12cafa8261f37cc8fb8b3c660abc1726e
Mangó <mango.shadowplay@gmail.com>
2024-11-29 12:30:01 +0100

    Add suggested changes

--- src/module/integration.js
+++ src/module/integration.js
@@ -3,3 +3,4 @@ export const MODULE_RG = "rollgroups"
 export const MODULE_DAE = "dae";
 export const MODULE_DSN = "dice-so-nice";
 export const MODULE_LIB = "lib-wrapper";
+export const MODULE_MIDI = "midi-qol";
